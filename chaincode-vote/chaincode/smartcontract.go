d0b971bf8ec786b87bb11ff74ac8434f2cc15b9d
jimwheaty <jimstaras@gmail.com>
2021-07-10 19:04:33 +0300

    ...

--- chaincode-vote/chaincode/smartcontract.go
+++ chaincode-vote/chaincode/smartcontract.go
@@ -16,45 +16,45 @@ type SmartContract struct {
 // }
 // Asset describes basic details of what makes up a simple asset
 type counter struct {
-	yes       int `json:"yes"`
-	no        int `json:"no"`
-	undefined int `json:"undefined"`
+	Yes       int `json:"yes"`
+	No        int `json:"no"`
+	Undefined int `json:"undefined"`
 }
 
 type board struct {
-	org1 bool `json:"org1"`
-	org2 bool `json:"org2"`
+	Org1 string `json:"org1"`
+	Org2 string `json:"org2"`
 }
 
 type vote struct {
-	id      string  `json:"id"`
-	message string  `json:"message"`
-	counter counter `json:"counter"`
-	board   board   `json:"board"`
+	ID      string  `json:"id"`
+	Message string  `json:"message"`
+	Counter counter `json:"counter"`
+	Board   board   `json:"board"`
 }
 
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) CreateVote(ctx contractapi.TransactionContextInterface) error {
 	vote := vote{
-		id:      "vote1",
-		message: "",
-		counter: counter{
-			yes:       0,
-			no:        0,
-			undefined: 2,
+		ID:      "vote1",
+		Message: "",
+		Counter: counter{
+			Yes:       0,
+			No:        0,
+			Undefined: 2,
 		},
-		board: board{
-			org1: nil,
-			org2: nil,
+		Board: board{
+			Org1: "",
+			Org2: "",
 		},
 	}
 
-	assetJSON, err := json.Marshal(vote)
+	voteJSON, err := json.Marshal(vote)
 	if err != nil {
 		return err
 	}
 
-	err = ctx.GetStub().PutState(vote.id, assetJSON)
+	err = ctx.GetStub().PutState(vote.ID, voteJSON)
 	if err != nil {
 		return fmt.Errorf("failed to put to world state. %v", err)
 	}
--- chaincode-vote/go.mod
+++ chaincode-vote/go.mod
@@ -1,4 +1,4 @@
-module github.com/jimwheaty/blockchainDAO
+module github.com/jimwheaty/blockchainDAO/chaincode-go
 
 go 1.16
 
