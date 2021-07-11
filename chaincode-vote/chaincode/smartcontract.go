c11f33f49fac804d4b31307e503352a7b4bd9e73
jimwheaty <jimstaras@gmail.com>
2021-07-11 19:21:27 +0300

    DoVote v1

--- chaincode-vote/chaincode/smartcontract.go
+++ chaincode-vote/chaincode/smartcontract.go
@@ -27,13 +27,14 @@ type board struct {
 }
 
 type vote struct {
-	ID      string  `json:"id"`
-	Message string  `json:"message"`
-	Counter counter `json:"counter"`
-	Board   board   `json:"board"`
+	ID         string  `json:"id"`
+	Message    string  `json:"message"`
+	Counter    counter `json:"counter"`
+	Board      board   `json:"board"`
+	IsFinished bool    `json:"isFinished"`
 }
 
-// InitLedger adds a base set of assets to the ledger
+// CreateVote initializes a vote to the ledger
 func (s *SmartContract) CreateVote(ctx contractapi.TransactionContextInterface) error {
 	vote := vote{
 		ID:      "vote1",
@@ -47,6 +48,7 @@ func (s *SmartContract) CreateVote(ctx contractapi.TransactionContextInterface) 
 			Org1: "",
 			Org2: "",
 		},
+		IsFinished: false,
 	}
 
 	voteJSON, err := json.Marshal(vote)
@@ -62,7 +64,7 @@ func (s *SmartContract) CreateVote(ctx contractapi.TransactionContextInterface) 
 	return nil
 }
 
-// ReadAsset returns the asset stored in the world state with given id.
+// ReadVote returns the vote stored in the world state with given id.
 func (s *SmartContract) ReadVote(ctx contractapi.TransactionContextInterface, id string) (*vote, error) {
 	voteJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
@@ -81,8 +83,8 @@ func (s *SmartContract) ReadVote(ctx contractapi.TransactionContextInterface, id
 	return &vote, nil
 }
 
-// UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateVote(ctx contractapi.TransactionContextInterface, id string, message string, counter counter, board board) error {
+// DoVote updates an existing vote in the world state with provided id and vote 'yes' or 'no'.
+func (s *SmartContract) DoVote(ctx contractapi.TransactionContextInterface, id string, vote_field string) error {
 	exists, err := s.VoteExists(ctx, id)
 	if err != nil {
 		return err
@@ -91,22 +93,49 @@ func (s *SmartContract) UpdateVote(ctx contractapi.TransactionContextInterface, 
 		return fmt.Errorf("the vote %s does not exist", id)
 	}
 
-	// overwriting original asset with new asset
-	vote := vote{
-		ID:      id,
-		Message: message,
-		Counter: counter,
-		Board:   board,
-	}
-	voteJSON, err := json.Marshal(vote)
-	if err != nil {
-		return err
-	}
+	if vote_field == "yes" || vote_field == "no" {
+		vote, err := s.ReadVote(ctx, id)
+		if err != nil {
+			return err
+		}
 
-	return ctx.GetStub().PutState(id, voteJSON)
+		counter := map[string]*int{
+			"yes": &vote.Counter.Yes,
+			"no":  &vote.Counter.No,
+			"":    &vote.Counter.Undefined,
+		}
+		board := map[string]*string{
+			"Org1": &vote.Board.Org1,
+			"Org2": &vote.Board.Org2,
+		}
+		address := "Org1"
+
+		*counter[*board[address]] -= 1
+		*board[address] = vote_field
+		*counter[*board[address]] += 1
+
+		if vote.Counter.Yes > (vote.Counter.No + vote.Counter.Undefined) {
+			vote.Message = "The vote is done. The result is Yes"
+			vote.IsFinished = true
+		} else if vote.Counter.No > (vote.Counter.Yes + vote.Counter.Undefined) {
+			vote.Message = "The vote is done. The result is No"
+			vote.IsFinished = true
+		} else {
+			vote.Message = "The vote is still going... No result yet."
+		}
+
+		voteJSON, err := json.Marshal(vote)
+		if err != nil {
+			return err
+		}
+
+		return ctx.GetStub().PutState(id, voteJSON)
+	} else {
+		return fmt.Errorf("failed to do vote: please use 'yes' or 'no' on your vote")
+	}
 }
 
-// AssetExists returns true when asset with given ID exists in world state
+// VoteExists returns true when vote with given ID exists in world state
 func (s *SmartContract) VoteExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	voteJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
