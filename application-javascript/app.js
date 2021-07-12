fdc2e438d2413a73aa8c195a77f0beb162501adf
jimwheaty <jimstaras@gmail.com>
2021-07-12 16:52:19 +0300

    my first application

--- application-javascript/app.js
+++ application-javascript/app.js
@@ -9,11 +9,11 @@
 const { Gateway, Wallets } = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
-const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../../test-application/javascript/CAUtil.js');
-const { buildCCPOrg1, buildWallet } = require('../../test-application/javascript/AppUtil.js');
+const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('./Utils/CAUtil');
+const { buildCCPOrg1, buildWallet } = require('./Utils/AppUtil');
 
 const channelName = 'mychannel';
-const chaincodeName = 'basic';
+const chaincodeName = 'vote';
 const mspOrg1 = 'Org1MSP';
 const walletPath = path.join(__dirname, 'wallet');
 const org1UserId = 'appUser';
@@ -114,59 +114,40 @@ async function main() {
 			// This type of transaction would only be run once by an application the first time it was started after it
 			// deployed the first time. Any updates to the chaincode deployed later would likely not need to run
 			// an "init" type function.
-			console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
-			await contract.submitTransaction('InitLedger');
+			console.log('\n--> Submit Transaction: CreateVote initializes a vote to the ledger');
+			await contract.submitTransaction('CreateVote');
 			console.log('*** Result: committed');
 
 			// Let's try a query type operation (function).
 			// This will be sent to just one peer and the results will be shown.
-			console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
-			let result = await contract.evaluateTransaction('GetAllAssets');
+			console.log('\n--> Evaluate Transaction: ReadVote returns the vote stored in the world state with given id.');
+			let result = await contract.evaluateTransaction('ReadVote', 'vote1');
 			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
 			// Now let's try to submit a transaction.
 			// This will be sent to both peers and if both peers endorse the transaction, the endorsed proposal will be sent
 			// to the orderer to be committed by each of the peer's to the channel ledger.
-			console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments');
-			result = await contract.submitTransaction('CreateAsset', 'asset13', 'yellow', '5', 'Tom', '1300');
+			console.log('\n--> Submit Transaction: DoVote updates an existing vote in the world state with provided id and vote field');
+			result = await contract.submitTransaction('DoVote', 'org1', 'vote1', 'yes');
 			console.log('*** Result: committed');
 			if (`${result}` !== '') {
 				console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 			}
 
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns an asset with a given assetID');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset13');
+			console.log('\n--> Evaluate Transaction: ReadVote returns the vote stored in the world state with given id.');
+			result = await contract.evaluateTransaction('ReadVote', 'vote1');
 			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
 
-			console.log('\n--> Evaluate Transaction: AssetExists, function returns "true" if an asset with given assetID exist');
-			result = await contract.evaluateTransaction('AssetExists', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			console.log('\n--> Submit Transaction: UpdateAsset asset1, change the appraisedValue to 350');
-			await contract.submitTransaction('UpdateAsset', 'asset1', 'blue', '5', 'Tomoko', '350');
-			console.log('*** Result: committed');
-
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns "asset1" attributes');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
-
-			try {
-				// How about we try a transactions where the executing chaincode throws an error
-				// Notice how the submitTransaction will throw an error containing the error thrown by the chaincode
-				console.log('\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error');
-				await contract.submitTransaction('UpdateAsset', 'asset70', 'blue', '5', 'Tomoko', '300');
-				console.log('******** FAILED to return an error');
-			} catch (error) {
-				console.log(`*** Successfully caught the error: \n    ${error}`);
-			}
-
-			console.log('\n--> Submit Transaction: TransferAsset asset1, transfer to new owner of Tom');
-			await contract.submitTransaction('TransferAsset', 'asset1', 'Tom');
-			console.log('*** Result: committed');
-
-			console.log('\n--> Evaluate Transaction: ReadAsset, function returns "asset1" attributes');
-			result = await contract.evaluateTransaction('ReadAsset', 'asset1');
-			console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+			// try {
+			// 	// How about we try a transactions where the executing chaincode throws an error
+			// 	// Notice how the submitTransaction will throw an error containing the error thrown by the chaincode
+			// 	console.log('\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error');
+			// 	await contract.submitTransaction('UpdateAsset', 'asset70', 'blue', '5', 'Tomoko', '300');
+			// 	console.log('******** FAILED to return an error');
+			// } catch (error) {
+			// 	console.log(`*** Successfully caught the error: \n    ${error}`);
+			// }
+			
 		} finally {
 			// Disconnect from the gateway when the application is closing
 			// This will close all connections to the network
--- test-network/refresh.sh
+++ test-network/refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./network.sh down
-./network.sh up createChannel
+./network.sh up createChannel -ca
 source export_enviromental_variables.sh
 
