8416192c887ad7673ac7d348e821644e53e8da26
jimwheaty <jimstaras@gmail.com>
2021-07-22 00:54:22 +0300

    added code for recalculating percentages

--- chaincode-vote/chaincode/smartcontract.go
+++ chaincode-vote/chaincode/smartcontract.go
@@ -3,6 +3,7 @@ package chaincode
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -72,7 +73,7 @@ func (s *SmartContract) ReadVote(ctx contractapi.TransactionContextInterface, id
 
 // DoVote updates an existing vote in the world state with provided id and vote 'yes' or 'no'.
 func (s *SmartContract) DoVote(ctx contractapi.TransactionContextInterface, address string, id string, vote_field string) error {
-	exists, err := s.VoteExists(ctx, id)
+	exists, err := s.voteExists(ctx, id)
 	if err != nil {
 		return err
 	}
@@ -94,8 +95,13 @@ func (s *SmartContract) DoVote(ctx contractapi.TransactionContextInterface, addr
 		vote.Counter[vote.Board[address]] += 1
 
 		if vote.Counter["yes"] > (vote.Counter["no"] + vote.Counter[""]) {
-			vote.Message = "The vote is done. The result is Yes"
+			vote.Message = "The vote is done. The result is Yes. \nCalculating percentages, please wait ..."
 			vote.IsFinished = true
+			percentageOrg1, percentageOrg2, err := s.calculatePercentages(ctx)
+			if err != nil {
+				return err
+			}
+			vote.Message = "The vote is done. The result is Yes. \nOrganization1: " + percentageOrg1 + "%, Organization2: " + percentageOrg2 + "%"
 		} else if vote.Counter["no"] > (vote.Counter["yes"] + vote.Counter[""]) {
 			vote.Message = "The vote is done. The result is No"
 			vote.IsFinished = true
@@ -114,8 +120,44 @@ func (s *SmartContract) DoVote(ctx contractapi.TransactionContextInterface, addr
 	}
 }
 
-// VoteExists returns true when vote with given ID exists in world state
-func (s *SmartContract) VoteExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+// calculatePercentages calculates the percentages for each organization of the energy data in the world state
+func (s *SmartContract) calculatePercentages(ctx contractapi.TransactionContextInterface) (percentageOrg1 string, percentageOrg2 string, error error) {
+	energyDataOrg1, err := s.getMonthlyData(ctx, "org1", "2021", "01")
+	if err != nil {
+		return "", "", err
+	}
+	energyDataOrg2, err := s.getMonthlyData(ctx, "org2", "2021", "01")
+	if err != nil {
+		return "", "", err
+	}
+
+	var energySumOrg1 float64
+	var energySumOrg2 float64
+	var datumFloat float64
+	for _, datum := range energyDataOrg1 {
+		datumFloat, err = strconv.ParseFloat(datum.Energy, 64)
+		if err != nil {
+			return "", "", err
+		}
+		energySumOrg1 += datumFloat
+	}
+	for _, datum := range energyDataOrg2 {
+		datumFloat, err = strconv.ParseFloat(datum.Energy, 64)
+		if err != nil {
+			return "", "", err
+		}
+		energySumOrg2 += datumFloat
+	}
+
+	energySum := energySumOrg1 + energySumOrg2
+	percentageOrg1 = strconv.FormatFloat(energySumOrg1/energySum, 'E', -1, 32)
+	percentageOrg2 = strconv.FormatFloat(energySumOrg2/energySum, 'E', -1, 32)
+
+	return percentageOrg1, percentageOrg2, nil
+}
+
+// voteExists returns true when vote with given ID exists in world state
+func (s *SmartContract) voteExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
 	voteJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
@@ -124,19 +166,52 @@ func (s *SmartContract) VoteExists(ctx contractapi.TransactionContextInterface, 
 	return voteJSON != nil, nil
 }
 
-// DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteVote(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.VoteExists(ctx, id)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return fmt.Errorf("the vote %s does not exist", id)
-	}
-
-	return ctx.GetStub().DelState(id)
+// energyData describes basic details of what makes up a simple energy data
+type energyData struct {
+	ID     string `json:"id"`
+	Energy string `json:"energy"`
 }
 
+// getMonthlyData returns all energy data for the given month found in world state
+func (s *SmartContract) getMonthlyData(ctx contractapi.TransactionContextInterface, org string, year string, month string) ([]*energyData, error) {
+	startKey := org + "." + year + month + "010000"
+	endKey := org + "." + year + month + "312345"
+	resultsIterator, err := ctx.GetStub().GetStateByRange(startKey, endKey)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var data []*energyData
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var datum energyData
+		err = json.Unmarshal(queryResponse.Value, &datum)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, &datum)
+	}
+
+	return data, nil
+}
+
+// // DeleteAsset deletes an given asset from the world state.
+// func (s *SmartContract) DeleteVote(ctx contractapi.TransactionContextInterface, id string) error {
+// 	exists, err := s.voteExists(ctx, id)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if !exists {
+// 		return fmt.Errorf("the vote %s does not exist", id)
+// 	}
+
+// 	return ctx.GetStub().DelState(id)
+// }
 // func (s *SmartContract) GetAddress(ctx contractapi.TransactionContextInterface) (address string, error error) {
 // 	creator, err := ctx.GetClientIdentity().GetID()
 // 	if err != nil {
