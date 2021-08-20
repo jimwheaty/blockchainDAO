cab03e2db9e283a3d78fd0376dd7f8a491f482f7
jimwheaty <jimstaras@gmail.com>
2021-08-20 13:59:49 +0300

    fixed postEnergyData bug

--- Design/postEnergyData.js
+++ Design/postEnergyData.js
@@ -2,56 +2,58 @@ fs = require('fs')
 fetch = require('node-fetch')
 
 let args = []
+let url = ''
+sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-postEnergyData = () => {
+postEnergyData = async () => {
     let orgName = args[0]
-    let url = ''
     if (orgName == 'org1') 
         url = 'http://localhost:10000/postEnergyData'
     else
         url = 'http://localhost:10001/postEnergyData'
     
     let fileName = 'diploma.energy.data.' + orgName + '.csv'
-    fs.readFile(fileName, 'utf8', (err,data) => {
+    fs.readFile(fileName, 'utf8', async (err,data) => {
         if (err)  
             return console.log(err)
         let lines = data.split('\n')
         lines = lines.slice(3)
-        let day = 0
-        lines.forEach(line => {
-            // sleep 1 second for every day. Otherwise the blockchain would block
-            // us as a malicious user
-            day += 1
-            if (day & 96 == 0) // 96 is the daily data count
-                new Promise(resolve => setTimeout(resolve, 1000)) 
+        // sleep 1 second for every day. Otherwise the blockchain would block
+        // us as a malicious user
+        for (let day=1; day<=31; day++){
+            await postEnergyDataPerDay(lines.slice(96*(day-1)+1, 96*day))
+            await sleep(1000)
+        }
+    })
+}
 
-            let [timestamp, energy] = line.split('\n')[0].split(', ')
-
-            const requestOptions = {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    "timestamp": timestamp.toString(),
-                    "energy" : energy.toString()
-                })
-            };
-            fetch(url, requestOptions)
-            .then(
-                (response) => {
-                    if (response.status !== 200) {
-                        console.log('Looks like there was a problem. Status Code: ' + response.status)
-                        return
-                    }
-
-                    // Examine the text in the response
-                    response.json().then((data) => {
-                        console.log(data);
-                    });
+postEnergyDataPerDay = (lines) => {
+    lines.forEach(line => {
+        let [timestamp, energy] = line.split('\n')[0].split(', ')
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "timestamp": timestamp.toString(),
+                "energy" : energy.toString()
+            })
+        }
+        fetch(url, requestOptions)
+        .then(
+            (response) => {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status)
+                    return
                 }
-            )
-            .catch(function(err) {
-                console.log('Fetch Error :-S', err);
-            });
+
+                // Examine the text in the response
+                response.json().then((data) => {
+                    console.log(data);
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
         })
     })
 }
